e49d76f1f94754813294c963604521c8f1bc1a8a
jason tlh <jasonjdh94@gmail.com>
2020-01-12 22:47:59 -0500

    committing for the night

--- client/src/components/Projects/Projects.js
+++ client/src/components/Projects/Projects.js
@@ -16,7 +16,7 @@ export default function Projects(props) {
         <div className="projects__info">
           <div className="projects__text">
             <h2 className="projects__name">Liquorish</h2>
-            <p className="projects__description">A catalog of cocktails that lets you to  search, filter, favorite, and upload personalized recipes. You can check pricing, ingredients, and instructions on how to mix your own drink.</p>
+            <p className="projects__description">A catalog of cocktails that lets you to  search, filter, favorite, and upload personalized recipes. You can check pricing, ingredients, and instructions on how to mix your own drink.<br/>Tech Stack: React, Node.js, Express.js, Multer, MongoDB</p>
           </div>
           <div className="projects__links">
             <a className="projects__github" href="https://github.com/JayTLH/liquorish" target="_blank" rel="noopener noreferrer">
@@ -26,7 +26,6 @@ export default function Projects(props) {
         </div>
       </div>
       <div className="projects__card">
-        <img className="projects__image" src={greatestPiano} alt="greatest piano homepage" />
         <div className="projects__info">
           <div className="projects__text">
             <h2 className="projects__name">Greatest Piano</h2>
@@ -38,6 +37,7 @@ export default function Projects(props) {
             </a>
           </div>
         </div>
+        <img className="projects__image" src={greatestPiano} alt="greatest piano homepage" />
       </div>
     </div>
   )
