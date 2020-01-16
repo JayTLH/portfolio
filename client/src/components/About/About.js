c305341d07b9a366b165c77cd5c507a7081acfe1
jason tlh <jasonjdh94@gmail.com>
2020-01-15 20:37:48 -0500

    most basic portfolio deployed

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -11,7 +11,7 @@ export default function About(props) {
       <h1 className="about__title">About</h1>
       <div className="about__info">
         <a className="about__credit" href='https://pngtree.com/so/domineering' target="_blank" rel="noopener noreferrer"><img className="about__image" src={dragon} alt="placeholder" /></a>
-        <p className="about__text">Toronto based web developer with a knack for solving problems<br />Constantly seeking new challenges, learning new development technologies<br />Fan of D&D, Critical Role</p>
+        <p className="about__text">Toronto based web developer with a knack for solving problems<br />Constantly seeking new challenges, learning new development technologies<br />Passionate about efficient code, fast load times and D&D</p>
       </div>
     </div>
   )
