00db26fb29a2733fb85f9bfdcad14b951cddfe68
jason tlh <jasonjdh94@gmail.com>
2020-03-27 20:52:13 -0400

    changed resume to be a link

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,7 +3,6 @@ import React from "react";
 
 // styles & assets
 import "./Contact.scss";
-import resume from "../../styles/assets/jason-huang-resume.pdf";
 import resumeLogo from "../../styles/assets/icons/resume.png";
 import email from "../../styles/assets/icons/email.png";
 import github from "../../styles/assets/icons/github.png";
@@ -17,7 +16,7 @@ export default function Contact(props) {
         <p className="contact__text">You can reach me through email or find me using the links below!</p>
         <div className="contact__logos">
           <a className="contact__link--two" href="mailto:jasonjdh94@gmail.com?subject=Hi%20Jason!"><img className="contact__link-img" src={email} alt="email logo" /></a>
-          <a className="contact__link--one" href={resume} target="_blank" rel="noopener noreferrer"><img className="contact__link-img" src={resumeLogo} alt="resume logo" /></a>
+          <a className="contact__link--one" href='https://resume.creddle.io/resume/r7k50mmwa' target="_blank" rel="noopener noreferrer"><img className="contact__link-img" src={resumeLogo} alt="resume logo" /></a>
           <a className="contact__link--three" href="https://github.com/JayTLH" target="_blank" rel="noopener noreferrer"><img className="contact__link-img" src={github} alt="github logo" /></a>
           <a className="contact__link--four" href="https://www.linkedin.com/in/jason-tlh/" target="_blank" rel="noopener noreferrer"><img className="contact__link-img" src={linkedin} alt="linkedin logo" /></a>
         </div>
